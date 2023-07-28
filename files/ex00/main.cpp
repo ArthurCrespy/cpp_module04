043b78500cb1228e0ce5c30f88825ac9b9e7d50f
CRESPY Arthur <arthur@crespy.eu>
2023-07-28 10:54:40 +0200

    Add ex00

---
+++ files/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                                                                   :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 15:09:31 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/14 15:59:54 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+    meta->makeSound();
+
+	const WrongAnimal* k = new WrongAnimal();
+	const WrongAnimal* l = new WrongCat();
+
+	std::cout << k->getType() << " " << std::endl;
+	std::cout << l->getType() << " " << std::endl;
+	l->makeSound();
+	k->makeSound();
+
+	return 0;
+}
