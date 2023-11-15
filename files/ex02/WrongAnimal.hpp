de07eeb7660b577897196514774fb3ee5ee8fedc
CRESPY Arthur <arthur@crespy.eu>
2023-11-15 06:07:16 +0100

    Add ex02

---
+++ files/ex02/WrongAnimal.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 15:09:31 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/14 15:59:54 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+# include <iostream>
+
+class WrongAnimal
+{
+	protected:
+		std::string type;
+	public:
+		WrongAnimal(void);
+		WrongAnimal(std::string type);
+		WrongAnimal(WrongAnimal const &src);
+		virtual ~WrongAnimal();
+
+		WrongAnimal &operator=(WrongAnimal const &rhs);
+
+		std::string getType() const;
+
+		void setType(std::string type);
+
+		virtual void makeSound() const;
+};
+
+#endif
